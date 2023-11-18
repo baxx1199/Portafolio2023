c71f7ac10584671c04904b6ca61116e2c26ac61d
baxx1199 <joshuajulcobaut@gmail.com>
2023-11-18 03:18:46 -0500

    maquetacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './vistas/footer/footer.component';
 import { NavbarComponent } from './vistas/navbar/navbar.component';
+import { AboutmeComponent } from './vistas/aboutme/aboutme.component';
+import { HeaderComponent } from './vistas/header/header.component';
+import { SkillsComponent } from './vistas/skills/skills.component';
+import { ProjectsComponent } from './vistas/projects/projects.component';
+import { ProjectCardComponent } from './componentes/project-card/project-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutmeComponent,
+    HeaderComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ProjectCardComponent
   ],
   imports: [
     BrowserModule,
